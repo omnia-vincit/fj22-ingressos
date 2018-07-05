c5bfd1730077a80e6ba8e38146fb01da4b1739df
Marcos Barbosa <lab7761@caelum141-11>
2018-07-04 22:49:55 -0300

    Commitando as alterações preço de sessão

--- src/main/java/br/com/caelum/ingresso/model/Sessao.java
+++ src/main/java/br/com/caelum/ingresso/model/Sessao.java
@@ -1,5 +1,7 @@
 package br.com.caelum.ingresso.model;
 
+import java.math.BigDecimal;
+import java.math.RoundingMode;
 import java.time.LocalTime;
 
 import javax.persistence.Entity;
@@ -9,19 +11,20 @@ import javax.persistence.ManyToOne;
 
 @Entity
 public class Sessao {
-    
+
 	@Id
 	@GeneratedValue
- 	private Integer id;
+	private Integer id;
 	private LocalTime horario;
 	@ManyToOne
 	private Sala sala;
 	@ManyToOne
 	private Filme filme;
-	
-	
-	public	Sessao()	{
-		
+
+	private BigDecimal preco;
+
+	public Sessao() {
+
 	}
 
 	public Sessao(LocalTime horario, Filme filme, Sala sala) {
@@ -65,10 +68,13 @@ public class Sessao {
 	public void setFilme(Filme filme) {
 		this.filme = filme;
 	}
-	
-	
-	
-	
-	
+
+	public BigDecimal getPreco() {
+		return preco.setScale(2, RoundingMode.HALF_UP);
+	}
+
+	public void setPreco(BigDecimal preco) {
+		this.preco = preco;
+	}
 
 }
